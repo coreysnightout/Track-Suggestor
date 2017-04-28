5ed9cf6c1806e55adea6848e6dede67cb183664c
Corey Redding <coreyredding@Coreys-iMac.local>
2017-04-28 14:11:14 -0700

    add if and else if functions to javascript page for index.html

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,22 @@ $(function() {
     var creative = $("select#creative").val();
     var php = $("select#php").val();
     var ruby = $("select#ruby").val();
-    var selector = ($('input[name=select]:checked').val());
+    var backEnd = ($('input[name=select]:checked').val());
 
-  })
+    if (age > 50 && creative === "yes" && php === "no" && ruby === "apple" && backEnd = "no" ) {
+      $(".css").show();
+    } else if (age < 50 && creative === "yes" && php === "yes" && ruby === "apple" && backEnd = "yes") {
+      $(".php").show();
+    } else if (age < 50 && creative === "no" && php === "no" && ruby === "android" && backEnd = "yes") {
+      $(".ruby").show();
+    } else if (age > 50 && creative === "yes" && php === "no" && ruby === "apple" && backEnd = "yes") {
+      $(".cSharp").val();
+    }
+
+
+
+    event.preventDefault();
+  });
 
 
 
