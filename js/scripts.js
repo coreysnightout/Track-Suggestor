3ea9a6d938289d112499ea6b6614bbcabb3646ce
Corey Redding <coreyredding@Coreys-iMac.local>
2017-04-28 12:01:32 -0700

    add javascript variable to functions and tweak index.html by defining classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 $(function() {
-  $("form#date").submit(function(event) {
-    $(".celeb").hide();
+  $("form#survey").submit(function(event){
+    $(".coding").hide();
+    var age = parseInt($("input#age").val());
+    var creative = $("select#creative").val();
+    var php = $(select#php).val();
+    var selector = ($('input[name=select]:checked').val());
+
+  })
+
+
+
+
+
+
+
+    // $(".coding").hide();
+    // var selector = ($('input[name=select]:checked').val());
+    // var age = parseInt($("input#age").val());
